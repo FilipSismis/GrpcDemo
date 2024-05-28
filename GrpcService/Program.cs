ae95746675ee4ac910f0d6813b2ce8e888cc3f44
Filip Šišmiš <filips@nextitsk.onmicrosoft.com>
2024-05-28 15:02:57 +0200

    added authorization

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,4 +1,6 @@
 using GrpcDemoService.Services;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("Entra"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<WeatherService>();
 app.MapGrpcService<NotifierService>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();
